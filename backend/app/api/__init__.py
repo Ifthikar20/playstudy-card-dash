45cb2a326c34ad48bdaee73d158983776e6dcc50
Ifthikar20 <ifthicaralikhan@gmail.com>
2025-12-25 12:07:41 -0600

    main changes to removing back end files

--- backend/app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-API endpoints package.
-"""
-from app.api import auth, app_data, study_sessions, questions
-
-__all__ = ["auth", "app_data", "study_sessions", "questions"]
--- backend/app/middleware/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Middleware package
-"""
-
-from .encryption_middleware import EncryptionMiddleware, get_decrypted_data
-
-__all__ = ["EncryptionMiddleware", "get_decrypted_data"]
